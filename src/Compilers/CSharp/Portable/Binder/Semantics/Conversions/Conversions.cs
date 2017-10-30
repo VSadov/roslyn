4f8ee55b99573839a0841035163025e0547a5e19
vsadov <vsadov@microsoft.com>
2017-10-30 13:59:41 -0700

    Fixes:#21979

--- src/Compilers/CSharp/Portable/Binder/Semantics/Conversions/Conversions.cs
+++ src/Compilers/CSharp/Portable/Binder/Semantics/Conversions/Conversions.cs
@@ -256,8 +256,8 @@ namespace Microsoft.CodeAnalysis.CSharp
                 return Conversion.NoConversion;
             }
 
-            //cannot capture span-like types.
-            if (!method.IsStatic && methodGroup.Receiver.Type.IsByRefLikeType)
+            //cannot capture stack-only types.
+            if (!method.IsStatic && methodGroup.Receiver.Type.IsRestrictedType())
             {
                 return Conversion.NoConversion;
             }
